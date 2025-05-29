b168543ad99faa356259eb86ef547e2d9f4485ee
Amiru Weerathunga <amiru.upek.weerathunga@gmail.com>
2025-05-29 23:18:24 +0530

    logo update resized
    + VerticalOffSet animation of gamepage list

--- Frost/Frost/Frost/HomePage.xaml.cs
+++ Frost/Frost/Frost/HomePage.xaml.cs
@@ -11,51 +11,6 @@ namespace Frost
         {
             this.InitializeComponent();
             this.DataContext = ViewModel;
-
-            // Optional: Populate dummy data or call methods to fetch system info
-            LoadSystemInfo();
-        }
-
-        private void LoadSystemInfo()
-        {
-            // Example: populate some drives info
-            ViewModel.Drives.Add(new DriveInfoModel
-            {
-                Name = "C:",
-                DriveType = "Fixed",
-                FileSystem = "NTFS",
-                TotalSizeFormatted = "512 GB",
-                FreeSpaceFormatted = "200 GB"
-            });
-            ViewModel.Drives.Add(new DriveInfoModel
-            {
-                Name = "D:",
-                DriveType = "Fixed",
-                FileSystem = "NTFS",
-                TotalSizeFormatted = "1 TB",
-                FreeSpaceFormatted = "750 GB"
-            });
-
-            // Example: populate drivers info
-            ViewModel.Drivers.Add(new DriverInfoModel
-            {
-                Name = "NVIDIA Graphics Driver",
-                Version = "527.56",
-                Provider = "NVIDIA",
-                Status = "Running"
-            });
-            ViewModel.Drivers.Add(new DriverInfoModel
-            {
-                Name = "Audio Driver",
-                Version = "10.0.19041",
-                Provider = "Realtek",
-                Status = "Running"
-            });
-
-            // Simulate CPU and Memory usage
-            ViewModel.CpuUsage = 42;
-            ViewModel.TotalMemory = 16384;
-            ViewModel.UsedMemory = 8200;
         }
     }
 }
